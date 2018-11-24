e12d9c03d22c15af06973e2878ea10406835f6d5
kru1980 <kru71101@gmail.com>
2018-11-24 09:01:50 +0500

    add respons cards

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -14,20 +14,25 @@ class Home extends Component {
         <Layout>
           <Content>
             <div style={{ padding: 24, background: "orange" }}>
-              {todos &&
-                todos.map(todo => {
-                  return (
-                    <Card
-                      title={todo.title}
-                      extra={<Link to={`/todo/${todo.id}`}>More</Link>}
-                      style={{ width: 300, marginBottom: 10 }}
-                      key={todo.id}
-                    >
-                      <p>author: {todo.author}</p>
-                      <p>dateTodoCompleted: {todo.dateTodoCompleted}</p>
-                    </Card>
-                  );
-                })}
+              {todos && (
+                <List
+                  grid={{
+                    gutter: 16,
+                    xs: 1,
+                    sm: 1,
+                    md: 2,
+                    lg: 3
+                    // xl: 6,
+                    // xxl: 3
+                  }}
+                  dataSource={todos}
+                  renderItem={item => (
+                    <List.Item>
+                      <Card title={item.title}>{item.title}</Card>
+                    </List.Item>
+                  )}
+                />
+              )}
             </div>
           </Content>
 
